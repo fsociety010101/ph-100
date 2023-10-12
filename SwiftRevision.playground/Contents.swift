6de5a46d53ff375e0343240515c3242884059cdb
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-12 19:59:47 +0200

    added swift revision playground

---
+++ SwiftRevision.playground/Contents.swift
@@ -0,0 +1,53 @@
+import Cocoa
+
+// Variables
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+// Constants
+let character = "Daphne"
+// cant redeclarate/change
+
+// Strings
+let actor = "Denzel Washington"
+
+let filename = "paris.jpg"
+
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let movie = """
+    A day in
+    the life of an
+    Apple engineer
+    """
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+
+print(filename.hasSuffix(".jpg"))
+
+
+// Numbers
+let number = 120
+let bigNumber = 100_000_000
+
+print(number.isMultiple(of: 3))
+
+print(120.isMultiple(of: 3))
+
+
+// Decimals
+
+
