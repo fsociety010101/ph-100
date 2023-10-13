d776dc4c66315a9c56b39729c18ac0930c6dbfb6
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-13 23:54:46 +0200

    completed part 1

--- SwiftRevision.playground/Contents.swift
+++ SwiftRevision.playground/Contents.swift
@@ -1,5 +1,6 @@
 import Cocoa
 
+
 // Variables
 var playerName = "Roy"
 print(playerName)
@@ -10,9 +11,12 @@ print(playerName)
 playerName = "Sam"
 print(playerName)
 
+
 // Constants
 let character = "Daphne"
 // cant redeclarate/change
+
+
 
 // Strings
 let actor = "Denzel Washington"
@@ -39,6 +43,7 @@ print(movie.hasPrefix("A day"))
 print(filename.hasSuffix(".jpg"))
 
 
+
 // Numbers
 let number = 120
 let bigNumber = 100_000_000
@@ -49,5 +54,19 @@ print(120.isMultiple(of: 3))
 
 
 // Decimals
+let a = 1
+let b = 2.0
 
+let c = a + b
 
+let c = a + Int(b)
+
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var rating = 5.0
+rating *= 2         // same range operators
