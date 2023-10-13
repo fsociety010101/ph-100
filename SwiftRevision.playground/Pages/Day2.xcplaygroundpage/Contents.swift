b9e8b71e86b63bc59269b6f223243c1f189adba0
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-10-14 00:36:51 +0200

    completed part 2

--- SwiftRevision.playground/Pages/Day2.xcplaygroundpage/Contents.swift
+++ SwiftRevision.playground/Pages/Day2.xcplaygroundpage/Contents.swift
@@ -2,6 +2,42 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+
+// Booleans
+let goodDogs = true
+let gameOver = false
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+let number = 120
+let isMultiple = number.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+isAuthenticated.toggle()
+print(isAuthenticated
+)
+
+
+
+// Strings
+let firstPart = "Hello"
+let secondPart = "world!"
+let greeting = firstPart + ", " + secondPart
+
+
+
+// String interpolation
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age)."
+print(message)
+
+
+let apolloNum = 11
+let missionMessage1 = "Apollo " + String(apolloNum)     // THIS IS SLOWER
+let missionMessage2 = "Apollo \(apolloNum)"             // THIS IS MUCH FASTER
 
 //: [Next](@next)
